82a36484b86754d9a3e9c4224b824d0effb461c6
shri <nshrunga27@gmail.com>
2025-09-10 22:24:04 -0700

    New repo and first program

---
+++ src/Lab001_SumOfNumbers.java
@@ -0,0 +1,6 @@
+public class Lab001_SumOfNumbers {
+    public static void main(String[] args) {
+        int a = 5, b = 7;
+        System.out.println("Sum of " + a + " and " + b + " is " +(a+b));
+    }
+}
