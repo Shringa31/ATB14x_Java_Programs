3d2436f5f85497e4b3c101b145f0fac02d7d35bd
shri <nshrunga27@gmail.com>
2025-09-21 15:32:15 -0700

    Add MONP code

---
+++ src/MaxOfNumberPackage.java
@@ -0,0 +1,10 @@
+public class MaxOfNumberPackage {
+    public static void main(String[] args) {
+        int a = 93;
+        int b = 94;
+        int c = 95;
+
+        int result = Math.max(a , Math.max(b,c));
+        System.out.println(result);
+    }
+}
